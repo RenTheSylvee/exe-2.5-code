64666e570086d7994f12baaad88da75e77bc2579
Ralsei <119281847+RalseiSmol03@users.noreply.github.com>
2023-03-28 18:24:05 +0800

    sus lua????

--- assets/preload/data/fight-or-flight/sus.lua
+++ assets/preload/data/fight-or-flight/sus.lua
@@ -1,24 +1,28 @@
 local bfx = 200;
 local bfy = 0;
 local zoomshit = 0;
+local dZoom = 1;
+local bZoom = 0.85;
 
 function onCreate()
---insert code here
+	-- insert your balls here lmao
 end
 
 function onMoveCamera(focus)
-    if focus == 'dad' then
-        setProperty('defaultCamZoom', 1)
-    elseif focus == 'boyfriend' then
-        setProperty('defaultCamZoom', 0.85)
-    end
+	if focus == 'dad' then
+		setProperty('defaultCamZoom', dZoom)
+	elseif focus == 'boyfriend' then
+		setProperty('defaultCamZoom', bZoom)
+	end
 end
 
 function onUpdate()
---fake 3d oooo
-    zoomshit = (getProperty('camGame.zoom')/0.75);
-    setCharacterX('boyfriend',bfx*zoomshit)
-    setCharacterY('boyfriend',bfy*zoomshit)
-    setProperty('boyfriend.scale.x',zoomshit)
-    setProperty('boyfriend.scale.y',zoomshit)
+	-- Fake 3D,they say XD
+	local cGZ = getProperty('camGame.zoom');
+	zoomshit = (cGZ / 0.75);
+	setCharacterX('boyfriend', bfx * zoomshit)
+	setCharacterY('boyfriend',bfy * zoomshit)
+	setProperty('boyfriend.scale.x', zoomshit)
+	setProperty('boyfriend.scale.y', zoomshit)
 end
+-- clean luascript
