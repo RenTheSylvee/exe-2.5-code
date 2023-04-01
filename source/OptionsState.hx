a9163dc7a9765a006629fb53d6c6849624a73832
Ralsei <119281847+RalseiSmol03@users.noreply.github.com>
2023-04-01 11:18:01 +0800

    update source/OptionsState.hx

--- source/OptionsState.hx
+++ source/OptionsState.hx
@@ -565,9 +565,9 @@ class PreferencesSubstate extends MusicBeatSubstate
 			}
 		}
 
-		#if mobile
-	  addVirtualPad(LEFT_FULL, A_B);
-		#end //it's a substate
+		/*#if mobile
+			addVirtualPad(LEFT_FULL, A_B);
+		#end*/ //it's a substate, but not necessary when baseoptions has virtpad
 
 		changeSelection();
 		reloadValues();
