86a23314ee403bd7a318751a14397885301de48b
MaysLastPlay10 <109207233+MaysLastPlay10@users.noreply.github.com>
2023-07-02 18:03:52 +0300

    v1

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -167,8 +167,8 @@ class TitleState extends MusicBeatState
 
 	var logoBl:FlxSprite;
 	var logoBlBUMP:FlxSprite;
-	var gfDance:FlxSprite;
-	var danceLeft:Bool = false;
+//	var gfDance:FlxSprite;
+//	var danceLeft:Bool = false;
 	var titleText:FlxSprite;
 	var bg:FlxSprite;
 	var swagShader:ColorSwap = null;
@@ -220,13 +220,13 @@ class TitleState extends MusicBeatState
 
 		add(logoBlBUMP);
 
-		gfDance = new FlxSprite(FlxG.width * 0.4, FlxG.height * 0.07);
+	/*	gfDance = new FlxSprite(FlxG.width * 0.4, FlxG.height * 0.07);
 		gfDance.frames = Paths.getSparrowAtlas('gfDanceTitle');
 		gfDance.animation.addByIndices('danceLeft', 'gfDance', [30, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14], "", 24, false);
 		gfDance.animation.addByIndices('danceRight', 'gfDance', [15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29], "", 24, false);
 		gfDance.antialiasing = true;
 		add(gfDance);
-		add(logoBl);
+		add(logoBl);*/
 
 		titleText = new FlxSprite(0, 0);
 		titleText.frames = Paths.getSparrowAtlas('titleEnterNEW');
